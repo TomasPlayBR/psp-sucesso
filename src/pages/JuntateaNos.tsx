89d12ab351379d8e1f3fb2a85295bef687fa4853
TomasPlayBR <tomasmvb099@gmail.com>
2026-03-27 00:03:34 +0000

    Update JuntateaNos.tsx

--- src/pages/JuntateaNos.tsx
+++ src/pages/JuntateaNos.tsx
@@ -61,7 +61,7 @@ export default function JuntateaNos() {
               A PSP do Sucesso RP procura jogadores responsáveis e dedicados para manter a ordem, o respeito e a atividade no servidor.
             </p>
             <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSf419JEX-aUwyMIaBXXg6Z4ujrM4Vm5RBH23UauxE23k8xyhw/viewform"
+              href="https://discord.com/channels/950196513110360094/1251175180265324725"
               target="_blank"
               rel="noopener noreferrer"
               className="btn-gold inline-flex items-center gap-2 text-base px-6 py-3"
